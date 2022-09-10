31428d30aec717afa43582b0e682d1e29758448a
mhakmi <marwahakmi@gmail.com>
2022-09-10 10:29:18 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books').then(resp => resp.json())
 }
 
 function renderBooks(books) {
@@ -13,5 +12,5 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks().then(books => renderBooks(books));
 });
